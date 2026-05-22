613a7b7453cc5b4f4bc142ce0501d2d8cbff90bd
Priyanshusriv023 <priyanshusri023@gmail.com>
2026-05-23 01:22:22 +0530

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,30 @@ import { SocialSection } from "@/modules/home/components/social-section";
 import { FacultyTestimonials } from "@/modules/home/components/faculty-testimonials";
 import { getAllBlogs } from "@/modules/blogs/lib/data";
 
+const shuffleArray = (array: string[]) => {
+  const shuffled = [...array];
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled;
+};
+
 export default function Home() {
   const allBlogs = getAllBlogs();
   const latestBlogs = allBlogs.slice(0, 7);
-  const galleryImages = allBlogs.map(b => b.image).filter(Boolean) as string[];
+  const galleryImages = shuffleArray([
+    '/forgallery/IMG_9815.jpg',
+    '/forgallery/IMG_9825.jpg',
+    '/forgallery/IMG_9827.jpg',
+    '/forgallery/IMG_9831.jpg',
+    '/forgallery/IMG_9859.jpg',
+    '/forgallery/IMG_9980.jpg',
+    '/forgallery/IMG_9984.jpg',
+    '/forgallery/_MG_9611.JPG',
+    '/forgallery/_MG_9834.JPG',
+    '/forgallery/_MG_9846.JPG',
+  ]);
 
   return (
     <main className="min-h-screen selection:bg-tertiary/30 selection:text-tertiary bg-surface text-on-surface flex flex-col">
